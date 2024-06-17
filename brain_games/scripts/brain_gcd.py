7f56e77d6aafdd449efea4e33cddbf97b36c37df
DaniilIslamgaliev <dante9226@mail.ru>
2024-06-17 22:41:02 +0300

    Add gcd script

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from brain_games.games import gcd as gcd_game
+
+
+from brain_games.engine import play
+
+
+def main():
+    play(gcd_game)
+
+
+if __name__ == '__main__'
+    main()
