76ce10e5b974ee53328bf696fe7fe26651742238
DaniilIslamgaliev <dante9226@mail.ru>
2024-06-17 23:06:03 +0300

    Fix gcd

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -7,11 +7,13 @@ from typing import NamedTuple
 Round = NamedTuple('Round', [('question', str), ('answer', str)])
 
 
-def find_gcd(a, b) -> int:
-    if a > b:
+
+def find_gcd(a: int, b: int):
+    if a < b:
         a, b = b, a
     if a == 0 or b == 0:
         return a or b
+
     return find_gcd(b, a % b)
 
 
@@ -26,4 +28,5 @@ def get_round():
 
     question = ''.join(map(str, (number1, number2)))
     gcd = find_gcd(number1, number2)
+
     return Round(question=question, answer=str(gcd))
