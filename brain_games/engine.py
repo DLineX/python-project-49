a23c091493122f4028b25449027a3d59c3bc4996
DaniilIslamgaliev <dante9226@mail.ru>
2024-06-17 21:41:47 +0300

    Fix engine

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -9,13 +9,13 @@ from brain_games.cli import welcome
 
 def play(game: ModuleType):
     welcome()
-    name = prompt('May I have your name?')
+    name = prompt.string('May I have your name?')
     print(f'Hello, {name}')
     game.show_rules()
     for _ in range(3):
         round = game.get_round()
         print(f'Question: {round.question}')
-        user_answer = prompt('Your answer:')
+        user_answer = prompt.string('Your answer: ')
         if user_answer != round.answer:
             print(f'{user_answer} is wrong answer ;(.'
                   f"Correct answer is '{round.answer}'.")
