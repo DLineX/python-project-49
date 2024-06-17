927999fa6608719f4eca58d2c79496c9976576d6
DaniilIslamgaliev <dante9226@mail.ru>
2024-06-17 22:21:03 +0300

    add calc script/game

---
+++ brain_games/games/calc.py
@@ -0,0 +1,27 @@
+from random import randint, choice
+
+
+from operator import add, sub, mul
+
+
+from typing import NamedTuple
+
+
+Round = NamedTuple('Round', [('question', str), ('answer', str)])
+
+
+def show_rules():
+    print('What the result of the exprassion?')
+
+
+def get_round():
+    upper_num = 100
+
+    operations = {' + ': add, ' - ': sub, ' * ': mul}
+    number1, number2 = randint(1, upper_num), randint(1, upper_num)
+    operation_symbol = choice(tuple(operations.keys()))
+
+    question = ''.join(map(str, (number1, operation_symbol, number2)))
+    result = operations[operation_symbol](number1, number2)
+
+    return Round(question=question, answer=str(result))
---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from brain_games.games import calc as calc_game
+
+
+from brain_games.engine import play
+
+
+def main():
+    play(calc_game)
+
+
+if __name__ == '__main__':
+    main()
