65c79f4574091d507f79811b756d0c71c48d7d63
DaniilIslamgaliev <dante9226@mail.ru>
2024-06-17 21:34:00 +0300

    add even game

---
+++ brain_games/games/even.py
@@ -0,0 +1,22 @@
+from random import randint
+
+
+from typing import NamedTuple
+
+
+Round = NamedTuple('Round', [('question', str), ('answer', str)])
+
+def show_rules():
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+
+
+def is_even(number: int):
+    return number % 2 == 0
+
+
+def get_round():
+    upper_num = 100
+
+    number = randint(0, upper_num)
+    answer = "yes" if is_even(number) else "no"
+    return Round(question=number, answer=answer)
